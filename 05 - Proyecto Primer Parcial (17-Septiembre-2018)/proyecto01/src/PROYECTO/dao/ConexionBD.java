47a1e06d670d7242aac76fbe10e01e1a813ac14f
Jesus Rodolfo <lopvaljr@gmail.com>
2023-02-02 12:58:30 -0600

    Subir proyecto a GIT

---
+++ 05 - Proyecto Primer Parcial (17-Septiembre-2018)/proyecto01/src/PROYECTO/dao/ConexionBD.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PROYECTO.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConexionBD {
+        protected Connection conn;
+    protected void cerrar(PreparedStatement stm)throws Exception{
+        stm.close();
+    }
+    protected void cerrar(ResultSet rst)throws Exception{
+        rst.close();
+    }
+    public ConexionBD() {
+        String driver="org.gjt.mm.mysql.Driver";
+        String user="root";
+        String pwd="";
+        String bd="proyecto01";
+        String url="jdbc:mysql://localhost/"+bd;
+        try {
+            Class.forName(driver);
+            conn=DriverManager.getConnection(url,user,pwd);
+            
+            
+        } catch (SQLException e) {
+            System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
